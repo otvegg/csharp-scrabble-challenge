0a410f685daf92be3a8970afae2da68bea7c2a9b
Oyvind Timian Dokk Husveg <oyvind.timian.dokk.husveg@no.experis.com>
2025-08-06 12:57:52 +0200

    Added Console UI

--- csharp-scrabble-challenge.Main/Program.cs
+++ csharp-scrabble-challenge.Main/Program.cs
@@ -2,14 +2,18 @@
 
 using csharp_scrabble_challenge.Main;
 
-Console.WriteLine("Welcome to Scrabble! To exit press q and Enter.");
+Console.WriteLine("Welcome to Scrabble");
+Console.WriteLine("Enter a word and get the score! Add a {} or [] around a letter or multiple letters to multiply score by 2 or 3 respectively");
+Console.WriteLine("To exit press q and Enter.");
 while  (true)
 {
-    Console.WriteLine("Enter a word and get the score! Add a {} or [] around a letter or multiple letters to multiply score by 2 or 3 respectively");
-    string word = Console.ReadLine();
     
+    string word = Console.ReadLine();
+    if (word == "q") break;
     Scrabble scrabble = new Scrabble(word);
-    scrabble.score();
+    Console.WriteLine($"{scrabble.score()}");
 
 
 }
+
+Console.WriteLine("Exiting... Thanks for playing!");
